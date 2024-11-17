6494599eb00d1519c2adec249b3a35471d9ca0da
Vignesh B <vignesh.b@justintegrateit.com>
2024-11-17 22:11:19 +0530

    SinglyLinkedList Insert Element At Start

--- LearnDSA/src/SinglyLinkedList/Runner.java
+++ LearnDSA/src/SinglyLinkedList/Runner.java
@@ -4,11 +4,22 @@ public class Runner {
 
     public static void main(String [] args)
     {
+        //Insert elements at the end like this 20 30 25 75
+        System.out.print("Insert Data At End : ");
         LinkedList list=new LinkedList();
         list.InsertDataEnd(list,20);
         list.InsertDataEnd(list,30);
         list.InsertDataEnd(list,25);
         list.InsertDataEnd(list,75);
         list.PrintNode(list);
+
+        //Insert elements at the start like this 75 25 30 20
+        System.out.print("Insert Data At Start : ");
+        LinkedList list2=new LinkedList();
+        list2.InsertDataStart(list2,20);
+        list2.InsertDataStart(list2,30);
+        list2.InsertDataStart(list2,25);
+        list2.InsertDataStart(list2,75);
+        list2.PrintNode(list2);
     }
 }
